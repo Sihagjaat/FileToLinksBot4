5119dcf32173588804888aa10ac62f0c80e35975
Sihagjaat <amitsihag504@gmail.com>
2026-01-22 16:00:23 +0530

    Update environment variables and configuration values

--- info.py
+++ info.py
@@ -11,51 +11,51 @@ API_HASH = environ.get('API_HASH', '')  # Telegram API Hash
 BOT_TOKEN = environ.get('BOT_TOKEN', '')  # Telegram Bot Token
 
 # 👑, Channels & Logs
-BIN_CHANNEL = int(environ.get("BIN_CHANNEL", "-1002143289748"))  # File storage channel
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002143289748"))  # General log channel
-PREMIUM_LOGS = int(environ.get("PREMIUM_LOGS", "-1002143289748"))  # Premium user actions log
-VERIFIED_LOG = int(environ.get("VERIFIED_LOG", "-1002143289748"))  # Verified user actions log
-SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", "-1002143289748"))
+BIN_CHANNEL = int(environ.get("BIN_CHANNEL", "-1003554063083"))  # File storage channel
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1003554063083"))  # General log channel
+PREMIUM_LOGS = int(environ.get("PREMIUM_LOGS", "-1003554063083"))  # Premium user actions log
+VERIFIED_LOG = int(environ.get("VERIFIED_LOG", "-1003554063083"))  # Verified user actions log
+SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", "-1003654058348"))
 
 # add admin IDs 11111 2222 3333 and add auth channel IDs -100XXX -100XXX -100XXX
-ADMINS = list(map(int, environ.get('ADMINS', '5123039648').split()))  # List of admin user IDs
-AUTH_CHANNEL = list(map(int, environ.get("AUTH_CHANNEL", "-1001861445521").split()))  # Allowed channels for authorization
+ADMINS = list(map(int, environ.get('ADMINS', '8467011403').split()))  # List of admin user IDs
+AUTH_CHANNEL = list(map(int, environ.get("AUTH_CHANNEL", "-1003568758638").split()))  # Allowed channels for authorization
 
 # username add without @
-OWNER_USERNAME = environ.get("OWNER_USERNAME", 'Sujan_Ch')  # Owner's username
-BOT_USERNAME = environ.get("BOT_USERNAME", 'FileStreamXProoBot')  # Bot's username
+OWNER_USERNAME = environ.get("OWNER_USERNAME", 'ContactToAdminToBot')  # Owner's username
+BOT_USERNAME = environ.get("BOT_USERNAME", 'PlusFileToLinkBot')  # Bot's username
 
 # 🔗 Channel & Support Links
-CHANNEL = environ.get('UPDATE CHANNEL', 'https://t.me/Sujan_Ch')  # Updates channel
-SUPPORT = environ.get('SUPPORT CHANNEL', 'https://t.me/Sujan_BotZ')  # Support group
-HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/')  # Verification guide link
-HOW_TO_OPEN = environ.get('HOW_TO_OPEN', 'https://t.me/')  # File access guide link
+CHANNEL = environ.get('UPDATE CHANNEL', 'https://t.me/FluxDrives')  # Updates channel
+SUPPORT = environ.get('Material CHANNEL', 'https://t.me/linkdebhaioffical')  # Support group
+HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/linkdebhaioffical')  # Verification guide link
+HOW_TO_OPEN = environ.get('HOW_TO_OPEN', 'https://t.me/linkdebhaioffical')  # File access guide link
 
 # ✅ Feature Toggles (True/False)
-VERIFY = environ.get("VERIFY", False)  # Enable user verification
+VERIFY = environ.get("VERIFY", True)  # Enable user verification
 FSUB = environ.get("FSUB", True)  # Force Subscribe feature
-ENABLE_LIMIT = environ.get("ENABLE_LIMIT", True)  # Enable file limits
+ENABLE_LIMIT = environ.get("ENABLE_LIMIT", False)  # Enable file limits
 BATCH_VERIFY = environ.get("BATCH_VERIFY", False)  # Verify files in batch
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))  # Enable channel shortlink creation
 MAINTENANCE_MODE = environ.get("MAINTENANCE_MODE", False)  # Put bot in maintenance
 PROTECT_CONTENT = environ.get('PROTECT_CONTENT', False)  # Enable content protection
-PUBLIC_FILE_STORE = environ.get('PUBLIC_FILE_STORE', False)  # Public or private file visibility
+PUBLIC_FILE_STORE = environ.get('PUBLIC_FILE_STORE', True)  # Public or private file visibility
 BATCH_PROTECT_CONTENT = environ.get('BATCH_PROTECT_CONTENT', False)  # Batch file protection
 
 # 🔗 Shortlink Configuration
-SHORTLINK_URL = environ.get('SHORTLINK_URL')  # Shortener site
-SHORTLINK_API = environ.get('SHORTLINK_API')  # API key for shortlink
+SHORTLINK_URL = environ.get('https://arolinks.com')  # Shortener site
+SHORTLINK_API = environ.get('2bd6b41b022d08c3d13cbe229497092a5c30cc7e')  # API key for shortlink
 
 # 💾 MongoDB Connection Information
-DB_URL = environ.get('DATABASE_URI', "mongodb+srv://sujanchy200:sujanchy200@cluster0.rxooijn.mongodb.net/?retryWrites=true&w=majority")  # MongoDB connection URI
+DB_URL = environ.get('DATABASE_URI', "")  # MongoDB connection URI
 DB_NAME = environ.get('DATABASE_NAME', "Sujan_BotZ")  # MongoDB database name
 
 # 📸 all Media (Images)
-QR_CODE = environ.get('QR_CODE')  # QR Code image
-VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")  # Verify success image
-AUTH_PICS = environ.get('AUTH_PICS', 'https://graph.org/file/721cd57c154c7f2a2b992-2bb665f9732ff027af.jpg')  # Auth step image
-PICS = environ.get('PICS', 'https://graph.org/file/366530995fd47a006743e-4b4d613a61e1feb9ce.jpg')  # Default info image
-FILE_PIC = environ.get('FILE_PIC', 'https://graph.org/file/721cd57c154c7f2a2b992-2bb665f9732ff027af.jpg') # file image 
+QR_CODE = environ.get('')  # QR Code image
+VERIFY_IMG = environ.get("VERIFY_IMG", "https://iwlwarpgyagzsmxnjudq.supabase.co/storage/v1/object/public/images/telegram/1769076845233_tfw2r.jpg")  # Verify success image
+AUTH_PICS = environ.get('AUTH_PICS', 'https://iwlwarpgyagzsmxnjudq.supabase.co/storage/v1/object/public/images/telegram/1769077188516_zjb0g4.jpg')  # Auth step image
+PICS = environ.get('PICS', 'https://iwlwarpgyagzsmxnjudq.supabase.co/storage/v1/object/public/images/telegram/1769077476262_0c05oh.jpg')  # Default info image
+FILE_PIC = environ.get('FILE_PIC', 'https://iwlwarpgyagzsmxnjudq.supabase.co/storage/v1/object/public/images/telegram/1768385814021_sa1t7k.jpg') # file image 
 
 # 📝 File Captions
 FILE_CAPTION = environ.get('FILE_CAPTION', f"{script.CAPTION}")  # Caption for single file
@@ -66,7 +66,7 @@ CHANNEL_FILE_CAPTION = environ.get('CHANNEL_FILE_CAPTION', f"{script.CAPTION}") 
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # Ping interval in seconds (20 minutes)
 SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))  # Threshold for sleep delay
 RATE_LIMIT_TIMEOUT = int(environ.get("RATE_LIMIT_TIMEOUT", "60"))  # Rate limit time (10 mins)
-MAX_FILES = int(environ.get("MAX_FILES", "5"))  # Max files allowed per user
+MAX_FILES = int(environ.get("MAX_FILES", "20"))  # Max files allowed per user
 VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 24))  # Time (in hours) after which verification expires
 
 # ⚙️ Worker Configuration
